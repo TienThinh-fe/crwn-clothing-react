7c61504c4746f88d5046c166500eca58ea6d1f8f
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-01-15 10:33:26 +0700

    fix: add styled-component and change style of component-item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Directory from "../../components/directory/directory.component";
 import { connect } from "react-redux";
 
-import "./homepage.styles.scss";
+import { HomePageContainer } from "./homepage.styles.jsx";
 
 const HomePage = (props) => {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <h1
         className="title"
         style={{ textAlign: "left", marginBottom: "40px", width: "100%" }}
@@ -14,7 +14,7 @@ const HomePage = (props) => {
         {props.currentUser ? `Welcome ${props.currentUser.displayName}!` : ""}
       </h1>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 };
 
