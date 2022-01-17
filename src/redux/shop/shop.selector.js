10273280016ad1a7a2ef40e9e8641a37cb7ec9b4
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-01-17 16:30:54 +0700

    fix: applied Container Pattern

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,14 +1,6 @@
 import memoize from "lodash.memoize";
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  women: 4,
-  men: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -31,4 +23,9 @@ export const selectCollection = memoize((collectionUrlParam) =>
 export const selectIsCollectionFetching = createSelector(
   [selectShop],
   (shop) => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  (shop) => !!shop.collections
 );
