509a6ebc393df97d8b258038a6ddfbfa445a93cc
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-01-17 16:10:47 +0700

    fix: applied redux-thunk into shop page

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -27,3 +27,8 @@ export const selectCollection = memoize((collectionUrlParam) =>
     collections ? collections[collectionUrlParam] : null
   )
 );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
