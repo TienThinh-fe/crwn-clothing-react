b5c16cca58e1cc8d40beb63812351051a45a1f46
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-01-20 10:54:06 +0700

    feature: separating client folder, add express server to handle stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckOut from "react-stripe-checkout";
+import axios from "axios";
 
 function StripeCheckoutButton({ price }) {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ function StripeCheckoutButton({ price }) {
     "pk_test_51KHkC9KD0R8n0m2vp26ldhqiUIo5Bc38DC3w6uV0rgJf9H3qMcVb1u4QcbpqKXOo3vU56koMgIspl2QMGH9XNzwl007XyUdHMP";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment successfully");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("successful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
@@ -17,7 +33,7 @@ function StripeCheckoutButton({ price }) {
       name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/updownload/file/script/d/f3eb2117da"
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is ${price}`}
       amount={priceForStripe}
       token={onToken}
